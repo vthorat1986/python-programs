e88f5b7069685320ee76342d5a956ffba5d80a13
Vaibhav Vijay Thorat <vaibhav.thorat1986@gmail.com>
2019-04-11 12:08:05 +0530

    Update upload_download_example.tf

--- upload_download_example.tf
+++ upload_download_example.tf
@@ -1,3 +1,7 @@
+data "local_file" "foo" {
+    filename = "$./assert.py"
+}
+
 resource "aws_s3_bucket" "first_bucket" {
   bucket = "first-vt-bucket-1511"
   acl = "private"
@@ -10,8 +14,8 @@ resource "aws_s3_bucket" "first_bucket" {
 resource "aws_s3_bucket_object" "object" {
   bucket = "first-vt-bucket-1511"
   key = "first_object"
-  source = "./assert.py"
-  etag = "${filemd5("./assert.py")}"
+  source = "${local_file.foo}"
+  etag = "${filemd5("${local_file.foo}")}"
 
   depends_on = ["aws_s3_bucket.first_bucket"]
 }
