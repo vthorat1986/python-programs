73f14631209294c6e5acb29dce943ad43ef81b46
Vaibhav Vijay Thorat <vaibhav.thorat1986@gmail.com>
2019-04-11 12:35:54 +0530

    Update upload_download_example.tf

--- upload_download_example.tf
+++ upload_download_example.tf
@@ -1,8 +1,12 @@
-resource "aws_s3_bucket" "first_bucket" {
+resource "local_file" "foo" {
+  filename = "${path.module}/assert.py"
+  
   provisioner "local-exec" {
     command = "dir /CloudReach/terraform_upload_download_example/*assert.py //s > ./assert.py"
-  }
-    
+  }  
+}
+
+resource "aws_s3_bucket" "first_bucket" {
   bucket = "first-vt-bucket-1511"
   acl = "private"
 
